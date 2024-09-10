b3e2102a07564edde13a5a8caa5e32dd53a1a099
mnaofficialbd <mna.official92@gmail.com>
2024-09-10 23:07:52 +0600

    ProvidingBestMedical component create and setup

---
+++ frontend/src/components/Home/ProvidingBestMedical.jsx
@@ -0,0 +1,96 @@
+import { BsArrowRight } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import icon01 from "../../assets/images/icon01.png";
+import icon02 from "../../assets/images/icon02.png";
+import icon03 from "../../assets/images/icon03.png";
+
+function ProvidingBestMedical() {
+  return (
+    <section>
+      <div className="container">
+        <div className="lg:w-[470px] mx-auto">
+          <h2 className="heading text-center">
+            Providing the best medical services
+          </h2>
+          <p className="text_para text-center">
+            World-class care for everyone. Our health system offers unmatched,
+            expert health care.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
+          {/* card-1 */}
+          <div className="py-[30px] px-5">
+            <div className="flex items-center justify-center">
+              <img src={icon01} alt="" />
+            </div>
+
+            <div className="mt-[30px]">
+              <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+                Find a Doctor
+              </h2>
+              <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+                World-class care for everyone. Our health system offers
+                unmatched, expert health care.
+              </p>
+
+              <Link
+                to="/doctors"
+                className="w-[44px] h-[44px] rounded-full border border-solid bottom-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+              >
+                <BsArrowRight className="group-hover:text-white w-6 h-6 " />
+              </Link>
+            </div>
+          </div>
+          {/* card-2 */}
+          <div className="py-[30px] px-5">
+            <div className="flex items-center justify-center">
+              <img src={icon02} alt="" />
+            </div>
+
+            <div className="mt-[30px]">
+              <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+                Find a Location
+              </h2>
+              <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+                World-class care for everyone. Our health system offers
+                unmatched, expert health care.
+              </p>
+
+              <Link
+                to="/doctors"
+                className="w-[44px] h-[44px] rounded-full border border-solid bottom-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+              >
+                <BsArrowRight className="group-hover:text-white w-6 h-6 " />
+              </Link>
+            </div>
+          </div>
+          {/* card-3 */}
+          <div className="py-[30px] px-5">
+            <div className="flex items-center justify-center">
+              <img src={icon03} alt="" />
+            </div>
+
+            <div className="mt-[30px]">
+              <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+                Book Appointment
+              </h2>
+              <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+                World-class care for everyone. Our health system offers
+                unmatched, expert health care.
+              </p>
+
+              <Link
+                to="/doctors"
+                className="w-[44px] h-[44px] rounded-full border border-solid bottom-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+              >
+                <BsArrowRight className="group-hover:text-white w-6 h-6 " />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ProvidingBestMedical;
