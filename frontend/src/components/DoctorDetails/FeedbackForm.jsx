73ece659e38a70b0822e2894a2184bb9a5968c20
mnaofficialbd <mna.official92@gmail.com>
2024-09-11 12:25:02 +0600

    FeedbackForm component create and setup

---
+++ frontend/src/components/DoctorDetails/FeedbackForm.jsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import { AiFillStar } from "react-icons/ai";
+import { useParams } from "react-router-dom";
+import PulseLoader from "react-spinners/PulseLoader";
+import { toast } from "react-toastify";
+import { BASE_URL, token } from "../../config";
+
+const FeedbackForm = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  const [reviewText, setReviewText] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const { id } = useParams();
+
+  const handleSubmitReview = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      if (!rating || !reviewText) {
+        setLoading(false);
+        return toast.error("Rating & Review fields are required");
+      }
+
+      const res = await fetch(`${BASE_URL}/doctors/${id}/reviews`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ rating, reviewText }),
+      });
+
+      const result = await res.json();
+      if (!res.ok) {
+        throw new Error(result.message);
+      }
+      setLoading(false);
+      toast.success(result.message);
+    } catch (err) {
+      setLoading(false);
+      toast.error(err.message);
+    }
+  };
+
+  console.log(reviewText);
+
+  return (
+    <form>
+      {/* feedback rating part */}
+      <div>
+        <h3 className="text-[16px] text-headingColor leading-6 font-semibold mb-4 mt-0">
+          How would you rate the overall experience?
+        </h3>
+        <div>
+          {[...Array(5).keys()].map((_, index) => {
+            index += 1;
+            return (
+              <button
+                key={index}
+                type="button"
+                className={`${
+                  index <= ((rating && hover) || hover)
+                    ? "text-yellowColor"
+                    : "text-gray-400"
+                } border-none bg-transparent outline-none text-[22px] cursor-pointer `}
+                onClick={() => setRating(index)}
+                onMouseEnter={() => setHover(index)}
+                onMouseLeave={() => setHover(rating)}
+                onDoubleClick={() => {
+                  setHover(0);
+                  setRating(0);
+                }}
+              >
+                <span>
+                  <AiFillStar />
+                </span>
+              </button>
+            );
+          })}
+        </div>
+      </div>
+
+      {/* feedback form part */}
+      <div className="mb-[30px]">
+        <h3 className="text-[16px] text-headingColor leading-6 font-semibold mb-4 mt-0">
+          Share your feedback or suggestions.
+        </h3>
+
+        <textarea
+          className="border border-solid border-[#0066ff34] focus:outline outline-primaryColor w-full px-4 py-3 rounded-md"
+          rows="5"
+          placeholder="Write your feedback"
+          onChange={(e) => setReviewText(e.target.value)}
+        ></textarea>
+      </div>
+
+      <button
+        className="btn hover:bg-slate-700 hover:border-none"
+        type="submit"
+        onClick={handleSubmitReview}
+      >
+        {loading ? <PulseLoader size={25} color="#fff" /> : "Submit Feedback"}
+      </button>
+    </form>
+  );
+};
+
+export default FeedbackForm;
