feaa2702351721b4ea6da994defea493329212a1
mnaofficialbd <mna.official92@gmail.com>
2024-09-13 00:02:39 +0600

    register page create and setup

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,219 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+// import HashLoader from "react-spinners/HashLoader";
+import PulseLoader from "react-spinners/PulseLoader";
+import { toast } from "react-toastify";
+import registerImg from "../assets/images/signup.gif";
+import { BASE_URL } from "../config.js";
+import uploadImageToCloudinary from "../utils/uploadCloudinary.js";
+
+const Register = () => {
+  const [selectFile, setSelectFile] = useState(null);
+  const [previewURL, setPreviewURL] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    photo: selectFile,
+    gender: "",
+    role: "patient",
+  });
+
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleFileInputChange = async (event) => {
+    const file = event.target.files[0];
+
+    // Use cloudinary to upload images
+    const data = await uploadImageToCloudinary(file);
+
+    setPreviewURL(data.url);
+    setSelectFile(data.url);
+    setFormData({ ...formData, photo: data.url });
+  };
+
+  // submitHandler
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+
+    try {
+      const res = await fetch(`${BASE_URL}/auth/register`, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const { message } = await res.json();
+
+      if (!res.ok) {
+        throw new Error(message);
+      }
+
+      setLoading(false);
+      toast.success(message);
+      navigate("/login");
+    } catch (error) {
+      toast.error(error.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <section className="px-5 xl:px-0 pt-8">
+      <div className="max-w-[1070px] mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+          {/* img box */}
+          <div className="hidden lg:block bg-primaryColor rounded-l-lg">
+            <figure className="rounded-l-lg">
+              <img src={registerImg} alt="" className="w-full rounded-l-lg" />
+            </figure>
+          </div>
+          {/* ===register card=== */}
+          <div className="rounded-l-lg lg:pl-16 pb-10">
+            <h3 className="text-[32px] text-headingColor text-center leading-9 font-bold mb-10">
+              Create an <span className="text-primaryColor">account</span>
+            </h3>
+
+            {/* ===register form=== */}
+            <form onSubmit={submitHandler}>
+              {/* name input */}
+              <div className="mb-5">
+                <input
+                  type="text"
+                  placeholder="Full Name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                  className="w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[22px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer"
+                  required
+                />
+              </div>
+              {/* email input */}
+              <div className="mb-5">
+                <input
+                  type="email"
+                  placeholder="Enter your Email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  className="w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[22px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer"
+                  required
+                />
+              </div>
+              {/* password input */}
+              <div className="mb-5">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  className="w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[22px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer"
+                  required
+                />
+              </div>
+              {/* ===selected part=== */}
+              <div className="mb-5 flex items-center justify-between">
+                {/* ===role select part=== */}
+                <label className="font-bold text-headingColor text-[16px] leading-7">
+                  Are you a:
+                  <select
+                    name="role"
+                    value={formData.role}
+                    onChange={handleInputChange}
+                    className="font-semibold text-textColor text-[15px] leading-7 px-4 py-3 focus:outline-none "
+                  >
+                    <option value="patient">Patient</option>
+                    <option value="doctor">Doctor</option>
+                  </select>
+                </label>
+
+                {/* ===gender select part=== */}
+                <label className="font-bold text-headingColor text-[16px] leading-7">
+                  Gender:
+                  <select
+                    name="gender"
+                    value={formData.gender}
+                    onChange={handleInputChange}
+                    className="font-semibold text-textColor text-[15px] leading-7 px-4 py-3 focus:outline-none "
+                  >
+                    <option value="">Select</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                  </select>
+                </label>
+              </div>
+              {/* ===img input=== */}
+              <div className="mb-5 flex items-center gap-5">
+                {selectFile && (
+                  <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center">
+                    <img
+                      src={previewURL}
+                      alt=""
+                      className="w-full rounded-full"
+                    />
+                  </figure>
+                )}
+                {/* ===upload img part=== */}
+                <div className="relative w-[130px] h-[50px]">
+                  <input
+                    type="file"
+                    name="photo"
+                    id="customFile"
+                    onChange={handleFileInputChange}
+                    accept=".jpg, .png, .jpeg"
+                    className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+                  />
+
+                  <label
+                    htmlFor="customFile"
+                    className="absolute top-0 left-0 w-full h-full flex items-center px-[0.75rem] py-[0.375rem] text-[15px] leading-6 overflow-hidden bg-[#0066ff46] text-headingColor font-semibold rounded-lg truncate cursor-pointer"
+                  >
+                    Upload Photo
+                  </label>
+                </div>
+              </div>
+
+              {/* ==register btn part== */}
+              <div className="mt-7">
+                <button
+                  disabled={loading && true}
+                  type="submit"
+                  className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3 hover:bg-slate-700 hover:border-none"
+                >
+                  {loading ? (
+                    // <HashLoader size={35} color="#ffffff" />
+                    <PulseLoader size={35} color="#ffffff" />
+                  ) : (
+                    "Register"
+                  )}
+                </button>
+              </div>
+              <p className="mt-5 text-textColor text-center">
+                Already have an account?{" "}
+                <Link
+                  to="/login"
+                  className="text-[16px] leading-7 font-medium text-primaryColor"
+                >
+                  Please Login
+                </Link>
+              </p>
+              {/* ==== */}
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Register;
