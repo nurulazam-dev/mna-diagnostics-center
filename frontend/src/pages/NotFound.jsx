0f28d9a460700137c0a5c03d6c31cdde147f5d2c
mnaofficialbd <mna.official92@gmail.com>
2024-09-12 12:36:22 +0600

    NotFound/404 page create and setup

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="mt-12">
+      <img src="https://i.ibb.co/q5F5b5v/404.png" alt="" className="w-full" />
+    </div>
+  );
+};
+
+export default NotFound;
