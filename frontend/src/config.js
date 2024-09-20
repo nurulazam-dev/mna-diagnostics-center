8d1ea5dc55da78690e5463624f59f23f33f6a221
mnaofficialbd <mna.official92@gmail.com>
2024-09-20 21:14:17 +0600

    cloudinary error solve-3

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "http://localhost:5000/api/v1";
 export const BASE_URL =
-  "http://mna-diagnostics-center-backend.onrender.com/api/v1";
+  "https://mna-diagnostics-center-backend.onrender.com/api/v1";
 export const token = localStorage.getItem("token");
