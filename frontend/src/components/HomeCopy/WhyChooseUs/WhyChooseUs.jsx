11476f42f8ef57bbcd62b694b611a50b0e33b529
mnaofficialbd <mna.official92@gmail.com>
2024-09-26 22:55:22 +0600

    ChooseUs section setup

---
+++ frontend/src/components/HomeCopy/WhyChooseUs/WhyChooseUs.jsx
@@ -0,0 +1,15 @@
+const WhyChooseUs = () => {
+  return (
+    <section>
+      <div className="container">
+        <div className="xl:w-[470px] mx-auto">
+          <h2 className="heading text-center">Why Choose US?</h2>
+        </div>
+        {/* ===Choose-List component=== */}
+        {/* <ChooseList /> */}
+      </div>
+    </section>
+  );
+};
+
+export default WhyChooseUs;
