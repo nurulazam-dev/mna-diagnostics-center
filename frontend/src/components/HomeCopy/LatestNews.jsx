83f565d666bc0b0c5ab01afaba125821b5594ec1
mnaofficialbd <mna.official92@gmail.com>
2024-09-27 12:48:48 +0600

    LatestNews component primary setup

--- frontend/src/components/HomeCopy/LatestNews.jsx
+++ frontend/src/components/HomeCopy/LatestNews.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { BsArrowRight } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { services } from "../../assets/data/servicesData";
 
 const LatestNews = () => {
-  return <div></div>;
+  return (
+    <section className="container my-5 pb-3 px-10">
+      <div className="">
+        <div className="xl:w-[470px] mx-auto">
+          <h2 className="heading text-center">Latest News</h2>
+        </div>
+        {/* ===Service-Section-List component=== */}
+        <div className="mt-[10px] lg:mt-[20px]">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 lg:gap-[20px]">
+            {services.slice(0, 3).map((service, index) => (
+              <div className="border rounded" key={index}>
+                <img src={service.serviceImg} alt="" />
+                <div className="pt-[5px] pb-[20px] px-3 lg:px-5 ">
+                  <h2 className="text-[22px] leading-6 font-[700]">
+                    {service.name}
+                  </h2>
+                  <p className="text-[14px] leading-6 text-textColor font-[400] mt-3">
+                    {service.desc}
+                  </p>
+
+                  <Link to="/about">
+                    <button className="flex items-center text-violet-700 mt-2">
+                      Read more{" "}
+                      <BsArrowRight className="group-hover:text-white w-6 h-6 ml-2" />
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default LatestNews;
--- frontend/src/pages/HomeCopy.jsx
+++ frontend/src/pages/HomeCopy.jsx
@@ -1,6 +1,7 @@
 import AdsBanner from "../components/HomeCopy/AdsBanner";
 import ContactSection from "../components/HomeCopy/ContactSection";
 import HeroSection from "../components/HomeCopy/HeroSection";
+import LatestNews from "../components/HomeCopy/LatestNews";
 import OurServices from "../components/HomeCopy/Services/OurServices";
 import WhyChooseUs from "../components/HomeCopy/WhyChooseUs";
 
@@ -11,6 +12,7 @@ const HomeCopy = () => {
       <WhyChooseUs />
       <OurServices />
       <AdsBanner />
+      <LatestNews />
       <ContactSection />
     </>
   );
