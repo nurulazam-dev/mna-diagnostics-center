c4a0dd80685378f4e27312a9b16d49256ae8cdb8
mnaofficialbd <mna.official92@gmail.com>
2024-09-27 12:26:08 +0600

    blog-data create

--- frontend/src/components/HomeCopy/AdsBanner.jsx
+++ frontend/src/components/HomeCopy/AdsBanner.jsx
@@ -5,7 +5,7 @@ const AdsBanner = () => {
   return (
     <section className="container bg-green-500 py-6 text-white">
       <div className="lg:flex block justify-around items-center">
-        <div className="">
+        <div>
           <h2 className="text-[44px] leading-[54px] font-[700]">
             How we can help...
           </h2>
