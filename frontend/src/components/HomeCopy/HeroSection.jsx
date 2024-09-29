f92251f5454404d3c0ea288e63426fb53336d2c2
mnaofficialbd <mna.official92@gmail.com>
2024-09-29 23:24:51 +0600

    hero section setup-4

--- frontend/src/components/HomeCopy/HeroSection.jsx
+++ frontend/src/components/HomeCopy/HeroSection.jsx
@@ -1,44 +1,42 @@
-import heroImg01 from "../../assets/images/others/feature-img.png";
+// import heroImg01 from "../../assets/images/others/feature-img.png";
+import heroImg01 from "../../assets/images/home/hero/heroImg.png";
 
 const HeroSection = () => {
   return (
-    <section className="pt-[30px] 2xl:h-[800px]">
-      <div className="container">
-        <div className="lg:flex justify-center items-center">
-          <div>
-            <div className="lg:w-[570px]">
-              <p className="text_para lg:text-[24px] text-[18px] font-bold mb-4">
-                Welcome to{" "}
-                <strong className="text-violet-700">
-                  MNA Diagnostics Center
-                </strong>{" "}
-              </p>
-              <h1 className="lg:text-[40px] text-[28px] font-[800] leading-[22px] lg:leading-[30px] text-headingColor md:text-[60px] md:leading-[70px]">
-                Your New Smile Start Here
+    <section className="container pt-[15px] 2xl:h-[800px] lg:px-10">
+      <div className="lg:flex justify-center items-center">
+        <div>
+          <div className="lg:w-[570px]">
+            <div className="lg:text-[40px] text-[28px] font-[800] leading-[22px] lg:leading-[30px]  md:text-[60px] md:leading-[70px]">
+              <h1>Welcome to</h1>
+              <h1 className="text-violet-700 font-mono mt-2">
+                MNA Diagnostics Center
               </h1>
-              <p className="text_para">
-                Your trusted partner in health. Book appointments with top
-                doctors and access a wide range of diagnostic services, all in
-                one place. Experience convenient, efficient, and personalized
-                care tailored to your needs. Your health, our priority.
-              </p>
-              <p className="text_para">
-                We are dedicated to providing personalized healthcare solutions
-                designed to meet your unique needs. Trust us to be your health
-                partner every step of the way, ensuring your well-being with
-                precision and care.
-              </p>
-              <div className="flex my-4 lg:mb-0">
-                <button className="customBtn bg-green-600 text-white mr-4">
-                  Request an Appointment
-                </button>
-                <button className="customBtn">Read More...</button>
-              </div>
+            </div>
+
+            <p className="text_para">
+              Your trusted partner in health. Book appointments with top doctors
+              and access a wide range of diagnostic services, all in one place.
+              Experience convenient, efficient, and personalized care tailored
+              to your needs. Your health, our priority.
+            </p>
+            <p className="text_para">
+              We are dedicated to providing personalized healthcare solutions
+              designed to meet your unique needs. Trust us to be your health
+              partner every step of the way, ensuring your well-being with
+              precision and care.
+            </p>
+            <div className="flex my-4 lg:mb-0">
+              <button className="customBtn bg-green-600 text-white mr-4">
+                Request an Appointment
+              </button>
+              <button className="customBtn">Read More...</button>
             </div>
           </div>
-          <div>
-            <img src={heroImg01} alt="" className="lg:max-w-md max-w-sm" />
-          </div>
+        </div>
+
+        <div>
+          <img src={heroImg01} alt="" />
         </div>
       </div>
     </section>
