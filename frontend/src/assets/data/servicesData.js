2d2a46d1e391fb4fe05d4252ee5481b25027d2e3
mnaofficialbd <mna.official92@gmail.com>
2024-10-02 11:47:29 +0600

    serices assets added and setup

--- frontend/src/assets/data/servicesData.js
+++ frontend/src/assets/data/servicesData.js
@@ -1,4 +1,13 @@
-import service1 from "../images/home/services/service1.png";
+import CardiologyImg from "../images/services/Cardiology.png";
+import DermatologyImg from "../images/services/Dermatology.png";
+import GastroenterologyImg from "../images/services/Gastroenterology.png";
+import GynecologyImg from "../images/services/Gynecology.png";
+import NeurologyImg from "../images/services/Neurology.png";
+import OncologyImg from "../images/services/Oncology.png";
+import OphthalmologyImg from "../images/services/Ophthalmology.png";
+import OrthopedicsImg from "../images/services/Orthopedics.png";
+import PediatricsImg from "../images/services/Pediatrics.png";
+import UrologyImg from "../images/services/Urology.png";
 
 export const services = [
   {
@@ -7,7 +16,7 @@ export const services = [
     desc: "Comprehensive heart care services, including diagnosis, treatment, and preventive cardiology.",
     bgColor: "rgba(255, 99, 132, 0.8)",
     textColor: "#FEB60D",
-    serviceImg: service1,
+    serviceImg: CardiologyImg,
   },
   {
     id: 2,
@@ -15,23 +24,23 @@ export const services = [
     desc: "Expert care for brain, spine, and nervous system disorders.",
     bgColor: "rgba(54, 162, 235, 0.8)",
     textColor: "#9771FF",
-    serviceImg: service1,
+    serviceImg: NeurologyImg,
   },
   {
     id: 3,
+    name: "Urology",
+    desc: "Specialized care for urinary and male reproductive health.",
+    bgColor: "rgba(153, 102, 255, 0.8)",
+    textColor: "#71a803",
+    serviceImg: UrologyImg,
+  },
+  {
+    id: 4,
     name: "Orthopedics",
     desc: "Comprehensive orthopedic care, including joint replacement, sports injuries, and fracture management.",
     bgColor: "rgba(75, 192, 192, 0.8)",
     textColor: "#068027",
-    serviceImg: service1,
-  },
-  {
-    id: 4,
-    name: "Pediatrics",
-    desc: "Specialized healthcare for infants, children, and adolescents.",
-    bgColor: "rgba(153, 102, 255, 0.8)",
-    textColor: "#802506",
-    serviceImg: service1,
+    serviceImg: OrthopedicsImg,
   },
   {
     id: 5,
@@ -39,7 +48,7 @@ export const services = [
     desc: "Advanced care for skin, hair, and nail disorders.",
     bgColor: "rgba(255, 159, 64, 0.8)",
     textColor: "#c9830a",
-    serviceImg: service1,
+    serviceImg: DermatologyImg,
   },
   {
     id: 6,
@@ -47,7 +56,7 @@ export const services = [
     desc: "Expert treatment for digestive system disorders, including liver and pancreas.",
     bgColor: "rgba(255, 206, 86, 0.8)",
     textColor: "#0a2ac9",
-    serviceImg: service1,
+    serviceImg: GastroenterologyImg,
   },
   {
     id: 7,
@@ -55,15 +64,15 @@ export const services = [
     desc: "Comprehensive eye care services, including surgery and vision correction.",
     bgColor: "rgba(75, 192, 192, 0.8)",
     textColor: "#6807f0",
-    serviceImg: service1,
+    serviceImg: OphthalmologyImg,
   },
   {
     id: 8,
-    name: "Gynecology",
-    desc: "Advanced care for women’s reproductive health and pregnancy.",
-    bgColor: "rgba(255, 99, 132, 0.8)",
-    textColor: "#a80398",
-    serviceImg: service1,
+    name: "Pediatrics",
+    desc: "Specialized healthcare for infants, children, and adolescents.",
+    bgColor: "rgba(153, 102, 255, 0.8)",
+    textColor: "#802506",
+    serviceImg: PediatricsImg,
   },
   {
     id: 9,
@@ -71,14 +80,14 @@ export const services = [
     desc: "Comprehensive cancer treatment and management services.",
     bgColor: "rgba(54, 162, 235, 0.8)",
     textColor: "#0366a8",
-    serviceImg: service1,
+    serviceImg: OncologyImg,
   },
   {
     id: 10,
-    name: "Urology",
-    desc: "Specialized care for urinary and male reproductive health.",
-    bgColor: "rgba(153, 102, 255, 0.8)",
-    textColor: "#71a803",
-    serviceImg: service1,
+    name: "Gynecology",
+    desc: "Advanced care for women’s reproductive health and pregnancy.",
+    bgColor: "rgba(255, 99, 132, 0.8)",
+    textColor: "#a80398",
+    serviceImg: GynecologyImg,
   },
 ];
