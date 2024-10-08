5558876f5eb10d0f9d9ab28541580eac827d5352
mnaofficialbd <mna.official92@gmail.com>
2024-10-08 12:26:18 +0600

    Awards component create-2

--- frontend/src/components/HomeCopy/Awards.jsx
+++ frontend/src/components/HomeCopy/Awards.jsx
@@ -2,6 +2,7 @@ const Awards = () => {
   return (
     <section className="p-4 lg:p-12">
       <h1 className="heading text-center">Winning Awards</h1>
+      <div className=""></div>
     </section>
   );
 };
