86197e5e8b67e6c84972545e5400c5f032201ba1
mnaofficialbd <mna.official92@gmail.com>
2024-10-10 00:19:59 +0600

    Awards data create-2

--- frontend/src/assets/data/awards.js
+++ frontend/src/assets/data/awards.js
@@ -1,21 +1,24 @@
-import awardImg01 from "../images/doctor-img01.png";
-import awardImg02 from "../images/doctor-img02.png";
-import awardImg03 from "../images/doctor-img03.png";
+import awardImg01 from "../images/home/awards/Excellence-in-Diagnosis-Award.png";
+import awardImg02 from "../images/home/awards/Healthcare-Hero-Award.png";
+import awardImg03 from "../images/home/awards/Top-Specialist-of-the-Year.png";
 
 export const awards = [
   {
     id: "01",
-    name: "",
+    name: "Excellence in Diagnosis Award",
+    time: "09 September, 2024",
     photo: awardImg01,
   },
   {
     id: "02",
-    name: "",
+    name: "Healthcare Hero Award",
+    time: "30 January, 2022",
     photo: awardImg02,
   },
   {
     id: "03",
-    name: "",
+    name: "Top Specialist of the Year",
+    time: "10 December, 2015",
     photo: awardImg03,
   },
 ];
--- frontend/src/components/HomeCopy/Awards.jsx
+++ frontend/src/components/HomeCopy/Awards.jsx
@@ -5,11 +5,18 @@ const Awards = () => {
     <section className="p-4 lg:p-12">
       <h1 className="heading text-center">Winning Awards</h1>
       {/* main div */}
-      <div className="">
+      <div className="flex justify-center mt-3">
         {/* card */}
         {awards.map((award) => (
-          <div key={award.id} className="">
-            <h2 className="">{award.name}</h2>
+          <div
+            key={award.id}
+            className="flex justify-between items-center border rounded px-3 mx-3"
+          >
+            <img src={award.photo} alt="" className="max-w-40" />
+            <div>
+              <h2 className="text-2xl font-semibold">{award.name}</h2>
+              <p className="">description</p>
+            </div>
           </div>
         ))}
       </div>
