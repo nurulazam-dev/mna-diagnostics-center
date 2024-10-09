de99c32083ffcc3339fc3e982c111b78f52193f1
mnaofficialbd <mna.official92@gmail.com>
2024-10-09 23:35:25 +0600

    Awards assets added

--- frontend/src/components/HomeCopy/Awards.jsx
+++ frontend/src/components/HomeCopy/Awards.jsx
@@ -1,8 +1,18 @@
+import { awards } from "../../assets/data/awards";
+
 const Awards = () => {
   return (
     <section className="p-4 lg:p-12">
       <h1 className="heading text-center">Winning Awards</h1>
-      <div className=""></div>
+      {/* main div */}
+      <div className="">
+        {/* card */}
+        {awards.map((award) => (
+          <div key={award.id} className="">
+            <h2 className="">{award.name}</h2>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
