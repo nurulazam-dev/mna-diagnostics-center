a6315607c3d488b224f4b871d880db6bcfc65f8f
mnaofficialbd <mna.official92@gmail.com>
2024-10-09 22:35:08 +0600

    Awards data create

---
+++ frontend/src/assets/data/awards.js
@@ -0,0 +1,21 @@
+import awardImg01 from "../images/doctor-img01.png";
+import awardImg02 from "../images/doctor-img02.png";
+import awardImg03 from "../images/doctor-img03.png";
+
+export const awards = [
+  {
+    id: "01",
+    name: "",
+    photo: awardImg01,
+  },
+  {
+    id: "02",
+    name: "",
+    photo: awardImg02,
+  },
+  {
+    id: "03",
+    name: "",
+    photo: awardImg03,
+  },
+];
