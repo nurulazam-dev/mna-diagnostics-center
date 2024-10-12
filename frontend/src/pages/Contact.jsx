b06461f9c589914336a7d1c31591013a67c51f54
mnaofficialbd <mna.official92@gmail.com>
2024-10-12 23:07:08 +0600

    countdown section create

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
             </div>
             {/* Contact Information */}
             <div className="mt-[2px]">
-              <h2 className="text-2xl font-bold text-gray-800">
+              <h2 className="lg:text-2xl text-xl font-bold text-violet-500">
                 Contact Information:
               </h2>
               <p className="mt-[3px] text-gray-600">
@@ -32,18 +32,18 @@ const Contact = () => {
 
           {/* Right Part: Contact Form */}
           <div className="w-full lg:w-3/5 px-8">
-            <h2 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+            <h2 className="text-xl lg:text-3xl font-bold text-gray-800 mb-4 text-center">
               Send Us a Message
             </h2>
-            <form className="space-y-4">
+            <form className="space-y-2">
               <div>
                 <label className="block text-sm font-medium text-gray-700 mb-1">
                   Email
                 </label>
                 <input
                   type="email"
-                  className="w-full px-4 py-2 border border-gray-300 rounded-sm focus:outline-none focus:ring-2 focus:ring-violet-600"
-                  placeholder="Enter your email"
+                  className="w-full px-4 py-3 border border-gray-300 rounded-sm focus:outline-none focus:ring-2 focus:ring-violet-600"
+                  placeholder="Enter Your Email"
                 />
               </div>
               <div>
@@ -52,7 +52,7 @@ const Contact = () => {
                 </label>
                 <input
                   type="text"
-                  className="w-full px-4 py-2 border border-gray-300 rounded-sm focus:outline-none focus:ring-2 focus:ring-violet-600"
+                  className="w-full px-4 py-3 border border-gray-300 rounded-sm focus:outline-none focus:ring-2 focus:ring-violet-600"
                   placeholder="Subject"
                 />
               </div>
@@ -67,7 +67,7 @@ const Contact = () => {
                 />
               </div>
               <div>
-                <button type="submit" className="w-full customBtn">
+                <button type="submit" className="w-full customBtn rounded-sm">
                   Submit
                 </button>
               </div>
