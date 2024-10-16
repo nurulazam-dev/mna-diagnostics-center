34e434975c34ccb55287d0e1887b87091855d637
mnaofficialbd <mna.official92@gmail.com>
2024-10-16 18:19:58 +0600

    management-team data setup

---
+++ frontend/src/assets/data/managementTeams.js
@@ -0,0 +1,125 @@
+import MTMemberImg from "../images/about/management-team/1.png";
+import MTMember12Img from "../images/about/management-team/12.png";
+import MTMember2Img from "../images/about/management-team/2.png";
+import MTMember3Img from "../images/about/management-team/3.png";
+import MTMember4Img from "../images/about/management-team/4.png";
+import MTMember5Img from "../images/about/management-team/5.png";
+import MTMember6Img from "../images/about/management-team/6.png";
+import MTMember7Img from "../images/about/management-team/7.png";
+
+export const managementTeams = [
+  {
+    id: "001",
+    name: "MNA Ariyan",
+    position: "Chairman",
+    photo: MTMember2Img,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "Managing Director (MD)",
+    photo: MTMemberImg,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "Chief Executive Officer (CEO)",
+    photo: MTMember3Img,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "MChief Medical Officer (CMO)",
+    photo: MTMember4Img,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "Chief Financial Officer (CFO)",
+    photo: MTMember5Img,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "Chief Operating Officer (COO)",
+    photo: MTMember6Img,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "Director of Operations",
+    photo: MTMember7Img,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "Director of Human Resources",
+    photo: MTMemberImg,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "Director of Marketing and Communications",
+    photo: MTMemberImg,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "Director of IT and Innovation",
+    photo: MTMemberImg,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "Head of Laboratory Services",
+    photo: MTMemberImg,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "Head of Patient Services",
+    photo: MTMember12Img,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "Head of Quality Assurance",
+    photo: MTMemberImg,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "Manager of Accounts and Finance",
+    photo: MTMemberImg,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "Manager of Administration",
+    photo: MTMemberImg,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "Manager of Sales and Business Development",
+    photo: MTMemberImg,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "Manager of Customer Relations",
+    photo: MTMemberImg,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "Manager of Procurement and Supplies",
+    photo: MTMemberImg,
+  },
+  {
+    id: "002",
+    name: "Dr. Mohammad Nurul Azam",
+    position: "Senior Consultant (Various Departments)",
+    photo: MTMemberImg,
+  },
+];
--- frontend/src/components/AboutUs/AboutManagementTeam.jsx
+++ frontend/src/components/AboutUs/AboutManagementTeam.jsx
@@ -1,100 +1,13 @@
-import MTMemberImg from "../../assets/images/about/management-team/1.png";
-import MTMember2Img from "../../assets/images/about/management-team/2.png";
-import MTMember3Img from "../../assets/images/about/management-team/3.png";
-import MTMember4Img from "../../assets/images/about/management-team/4.png";
-import MTMember5Img from "../../assets/images/about/management-team/5.png";
-import MTMember6Img from "../../assets/images/about/management-team/6.png";
-import MTMember7Img from "../../assets/images/about/management-team/7.png";
+import { managementTeams } from "../../assets/data/managementTeams";
 
 const AboutManagementTeam = () => {
-  const managementTeam = [
-    {
-      id: "001",
-      name: "MNA Ariyan",
-      position: "Chairman",
-      photo: MTMember2Img,
-    },
-    {
-      id: "002",
-      name: "Dr. Mohammad Nurul Azam",
-      position: "Managing Director",
-      photo: MTMemberImg,
-    },
-    {
-      id: "002",
-      name: "Dr. Mohammad Nurul Azam",
-      position: "Managing Director",
-      photo: MTMember3Img,
-    },
-    {
-      id: "002",
-      name: "Dr. Mohammad Nurul Azam",
-      position: "Managing Director",
-      photo: MTMember4Img,
-    },
-    {
-      id: "002",
-      name: "Dr. Mohammad Nurul Azam",
-      position: "Managing Director",
-      photo: MTMember5Img,
-    },
-    {
-      id: "002",
-      name: "Dr. Mohammad Nurul Azam",
-      position: "Managing Director",
-      photo: MTMember6Img,
-    },
-    {
-      id: "002",
-      name: "Dr. Mohammad Nurul Azam",
-      position: "Managing Director",
-      photo: MTMember7Img,
-    },
-    {
-      id: "002",
-      name: "Dr. Mohammad Nurul Azam",
-      position: "Managing Director",
-      photo: MTMemberImg,
-    },
-    {
-      id: "002",
-      name: "Dr. Mohammad Nurul Azam",
-      position: "Managing Director",
-      photo: MTMemberImg,
-    },
-    {
-      id: "002",
-      name: "Dr. Mohammad Nurul Azam",
-      position: "Managing Director",
-      photo: MTMemberImg,
-    },
-    {
-      id: "002",
-      name: "Dr. Mohammad Nurul Azam",
-      position: "Managing Director",
-      photo: MTMemberImg,
-    },
-    {
-      id: "002",
-      name: "Dr. Mohammad Nurul Azam",
-      position: "Managing Director",
-      photo: MTMemberImg,
-    },
-    {
-      id: "002",
-      name: "Dr. Mohammad Nurul Azam",
-      position: "Managing Director",
-      photo: MTMemberImg,
-    },
-  ];
-
   return (
     <section className="pb-2">
       <h2 className="text-2xl font-bold text-center mb-2">
         Meet Our Management Team
       </h2>
       <div className="grid gap-5 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
-        {managementTeam.map((member) => (
+        {managementTeams.map((member) => (
           <div
             key={member.id}
             className="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden transform transition hover:-translate-y-2 duration-300"
@@ -108,7 +21,9 @@ const AboutManagementTeam = () => {
               <h3 className="text-[16px] lg:text-[18px] font-semibold text-gray-800">
                 {member.name}
               </h3>
-              <p className="text-gray-600">{member.position}</p>
+              <p className="text-gray-600  text-[12px] lg:text-[14px]">
+                {member.position}
+              </p>
             </div>
           </div>
         ))}
