0854801b6e6fabb510ea7b886e28c58f4cebb791
mnaofficialbd <mna.official92@gmail.com>
2024-10-18 22:02:27 +0600

    myAccount page primary setup complete

--- frontend/src/Dashboard/user-account/MyAccount.jsx
+++ frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import userAvatar from "../../assets/images/icons/patient-avatar.png";
 import Error from "../../components/Shared/Error";
 import Loading from "../../components/Shared/Loading";
 import { BASE_URL } from "../../config";
@@ -18,78 +19,74 @@ const MyAccount = () => {
   console.log(userData);
 
   return (
-    <section>
-      <div className="max-w-[1170px] px-5 mx-auto">
-        {loading && !error && <Loading />}
+    <section className="max-w-[1170px] px-5 mx-auto my-6">
+      {loading && !error && <Loading />}
 
-        {error && !loading && <Error errMessage={error} />}
+      {error && !loading && <Error errMessage={error} />}
 
-        {!loading && !error && (
-          <div className="grid md:grid-cols-3 gap-10">
-            {/* ===left-side part=== */}
-            <div className="pb-[50px] px-[30px] rounded-md">
-              <div className="flex items-center justify-center">
-                <figure className="w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor">
-                  <img
-                    src={userData.photo}
-                    alt=""
-                    className="w-full h-full rounded-full"
-                  />
-                </figure>
-              </div>
-
-              <div className="text-center mt-4">
-                <h3 className="text-[18px] leading-[30px] text-headingColor font-bold">
-                  {userData.name}
-                </h3>
-                <p className="text-[15px] leading-6 text-textColor font-medium">
-                  {userData.email}
-                </p>
-                <p className="text-[15px] leading-6 text-textColor font-medium">
-                  Blood Group:{" "}
-                  <span className="text-[22px] leading-8 text-headingColor ml-2">
-                    {userData.bloodGroup}
-                  </span>
-                </p>
-              </div>
-
-              <div className="mt-[50px] md:mt-[100px]">
-                <button className="text-[16px] leading-7 rounded-md w-full bg-red-600 mt-4 text-white p-3">
-                  Delete Account
-                </button>
-              </div>
+      {!loading && !error && (
+        <div className="grid md:grid-cols-3 gap-10">
+          {/* ===left-side part=== */}
+          <div className="pb-[50px] px-[30px] rounded-md shadow-md">
+            <div className="flex items-center justify-center">
+              <figure className="w-[100px] h-[100px] rounded-full border-2 border-solid border-violet-700">
+                <img
+                  src={userData.photo ? userData.photo : userAvatar}
+                  alt=""
+                  className="w-full h-full rounded-full"
+                />
+              </figure>
             </div>
 
-            {/* ===Right-side part=== */}
-            <div className="md:col-span-2 md:px-[30px]">
-              <div>
-                <button
-                  onClick={() => setTab("bookings")}
-                  className={`${
-                    tab == "bookings" &&
-                    "bg-primaryColor text-white font-normal"
-                  } p-2 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px] leading-7 border border-solid border-primaryColor`}
-                >
-                  My Bookings
-                </button>
-                <button
-                  onClick={() => setTab("settings")}
-                  className={`${
-                    tab == "settings" &&
-                    "bg-primaryColor text-white font-normal"
-                  } py-2 px-5 rounded-md text-headingColor font-semibold text-[16px] leading-7 border border-solid border-primaryColor`}
-                >
-                  Profile Settings
-                </button>
-              </div>
+            <div className="text-center mt-4">
+              <h3 className="text-[18px] leading-[30px] text-headingColor font-bold">
+                {userData.name}
+              </h3>
+              <p className="text-[15px] leading-6 text-textColor font-medium">
+                {userData.email}
+              </p>
+              <p className="text-[15px] leading-6 text-textColor font-medium">
+                Blood Group:{" "}
+                <span className="text-[22px] leading-8 text-headingColor ml-2">
+                  {userData.bloodGroup}
+                </span>
+              </p>
+            </div>
 
-              {/* ===active tab show_details=== */}
-              {tab == "bookings" && <MyBookings />}
-              {tab == "settings" && <Profile user={userData} />}
+            <div className="mt-[50px] md:mt-[100px]">
+              <button className="text-[16px] leading-7 rounded-md w-full bg-red-600 mt-4 text-white p-3">
+                Delete Account
+              </button>
             </div>
           </div>
-        )}
-      </div>
+
+          {/* ===Right-side part=== */}
+          <div className="md:col-span-2 md:px-[30px]">
+            <div>
+              <button
+                onClick={() => setTab("bookings")}
+                className={`${
+                  tab == "bookings" && "bg-green-600 text-white"
+                } mr-5 customBtn`}
+              >
+                My Bookings
+              </button>
+              <button
+                onClick={() => setTab("settings")}
+                className={`${
+                  tab == "settings" && "bg-green-600 text-white"
+                } customBtn`}
+              >
+                Profile Settings
+              </button>
+            </div>
+
+            {/* ===active tab show_details=== */}
+            {tab == "bookings" && <MyBookings />}
+            {tab == "settings" && <Profile user={userData} />}
+          </div>
+        </div>
+      )}
     </section>
   );
 };
