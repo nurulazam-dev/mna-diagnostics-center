69933f6cee075af511c937fcb7b3b79da7f77322
mnaofficialbd <mna.official92@gmail.com>
2024-10-29 23:30:13 +0600

    doctor dashboard update-2

--- frontend/src/Dashboard/doctor-account/Appointments.jsx
+++ frontend/src/Dashboard/doctor-account/Appointments.jsx
@@ -3,77 +3,81 @@ import { formatDate } from "../../utils/formatDate";
 /* eslint-disable react/prop-types */
 const Appointments = ({ appointments }) => {
   return (
-    <table className="w-full text-left text-sm text-green-500">
-      <h1 className="text-[28px] font-serif text-center text-black font-bold mb-2">
+    <section className="">
+      <h1 className="text-[28px] font-serif text-center text-black font-bold mb-3">
         Appointments
       </h1>
-      <thead>
-        <tr className="text-sm text-violet-600 text-center uppercase hidden md:table-row bg-gray-100">
-          <th className="px-6 py-3">Patient</th>
-          <th className="px-6 py-3">Gender</th>
-          <th className="px-6 py-3">Payment</th>
-          <th className="px-6 py-3">Price</th>
-          <th className="px-6 py-3">Booked on</th>
-        </tr>
-      </thead>
-
-      <tbody className="text-textColor">
-        {appointments?.map((item) => (
-          <tr
-            key={item._id}
-            className="md:table-row border-b border-gray-200 hover:bg-gray-100 block mb-4 lg:mb-0"
-          >
-            <td className="flex items-center  lg:px-6 px-2 lg:py-4 text-gray-900 whitespace-nowrap mb-2 lg:mb-0">
-              <img
-                src={item.user.photo}
-                alt=""
-                className="w-10 h-10 rounded-full"
-              />
-              <div className="pl-3">
-                <div className="text-base font-semibold">{item.user.name} </div>
-                <div className="text-normal text-gray-500">
-                  {item.user.email}{" "}
-                </div>
-              </div>
-            </td>
-
-            <td className="lg:px-6 px-2 lg:py-4 md:table-cell block">
-              <span className="font-bold md:hidden">Gender: </span>
-              {item.user.gender}
-            </td>
-
-            <td className="lg:px-6 px-2 lg:py-4 md:table-cell flex  items-center">
-              <span className="font-bold md:hidden mr-1">Payment: </span>
-              {item.isPaid && (
-                <div className="flex justify-center items-center lg:bg-green-200 text-green-600 py-1 px-[9px] rounded-full text-[12px]">
-                  <div className="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div>{" "}
-                  Paid
-                </div>
-              )}
-              {!item.isPaid && (
-                <div className="flex justify-center items-center lg:bg-red-200 text-red-600 py-1 px-[9px] rounded-full text-[12px]">
-                  <div className="h-2.5 w-2.5 rounded-full bg-red-500 mr-2"></div>{" "}
-                  Unpaid
-                </div>
-              )}
-            </td>
-            <td className="lg:px-6 px-2 lg:py-4 md:table-cell block">
-              <span className="font-bold md:hidden">Price: </span>
-              {item.ticketPrice}
-            </td>
-            <td className="lg:px-6 px-2 lg:py-4 md:table-cell block">
-              <span className="font-bold md:hidden">Booked On: </span>
-              {formatDate(item.createdAt)}
-            </td>
+      <table className="w-full text-left text-sm text-green-500">
+        <thead>
+          <tr className="text-sm text-violet-600 text-center uppercase hidden md:table-row bg-gray-100">
+            <th className="px-6 py-3">Patient</th>
+            <th className="px-6 py-3">Gender</th>
+            <th className="px-6 py-3">Payment</th>
+            <th className="px-6 py-3">Price</th>
+            <th className="px-6 py-3">Booked on</th>
           </tr>
-        ))}
-      </tbody>
-      {appointments.length == 0 && (
-        <h2 className="text-red-600 animate-pulse text-[16px] my-3">
-          Not booked any appointment.
-        </h2>
-      )}
-    </table>
+        </thead>
+
+        <tbody className="text-textColor">
+          {appointments?.map((item) => (
+            <tr
+              key={item._id}
+              className="md:table-row border-b border-gray-200 hover:bg-gray-100 block mb-4 lg:mb-0"
+            >
+              <td className="flex items-center  lg:px-6 px-2 lg:py-4 text-gray-900 whitespace-nowrap mb-2 lg:mb-0">
+                <img
+                  src={item.user.photo}
+                  alt=""
+                  className="w-10 h-10 rounded-full"
+                />
+                <div className="pl-3">
+                  <div className="text-base font-semibold">
+                    {item.user.name}{" "}
+                  </div>
+                  <div className="text-normal text-gray-500">
+                    {item.user.email}{" "}
+                  </div>
+                </div>
+              </td>
+
+              <td className="lg:px-6 px-2 lg:py-4 md:table-cell block">
+                <span className="font-bold md:hidden">Gender: </span>
+                {item.user.gender}
+              </td>
+
+              <td className="lg:px-6 px-2 lg:py-4 md:table-cell flex  items-center">
+                <span className="font-bold md:hidden mr-1">Payment: </span>
+                {item.isPaid && (
+                  <div className="flex justify-center items-center lg:bg-green-200 text-green-600 py-1 px-[9px] rounded-full text-[12px]">
+                    <div className="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div>{" "}
+                    Paid
+                  </div>
+                )}
+                {!item.isPaid && (
+                  <div className="flex justify-center items-center lg:bg-red-200 text-red-600 py-1 px-[9px] rounded-full text-[12px]">
+                    <div className="h-2.5 w-2.5 rounded-full bg-red-500 mr-2"></div>{" "}
+                    Unpaid
+                  </div>
+                )}
+              </td>
+              <td className="lg:px-6 px-2 lg:py-4 md:table-cell block">
+                <span className="font-bold md:hidden">Price: </span>
+                {item.ticketPrice}
+              </td>
+              <td className="lg:px-6 px-2 lg:py-4 md:table-cell block">
+                <span className="font-bold md:hidden">Booked On: </span>
+                {formatDate(item.createdAt)}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        {appointments.length == 0 && (
+          <h2 className="text-red-600 animate-pulse text-[16px] my-3">
+            Not booked any appointment.
+          </h2>
+        )}
+      </table>
+    </section>
   );
 };
 
--- frontend/src/Dashboard/doctor-account/Dashboard.jsx
+++ frontend/src/Dashboard/doctor-account/Dashboard.jsx
@@ -1,106 +1,154 @@
 import { useState } from "react";
+import { CgDanger } from "react-icons/cg";
+import starIcon from "../../assets/images/icons/Star.png";
+import doctorAvatar from "../../assets/images/icons/avatar-icon.png";
+import DoctorAbout from "../../components/DoctorDetails/DoctorAbout";
 import Error from "../../components/Shared/Error";
 import Loading from "../../components/Shared/Loading";
 import { BASE_URL } from "../../config";
 import useGetProfile from "../../hooks/useFetchData";
+import Appointments from "./Appointments";
+import Profile from "./Profile";
+
+import { BiSolidUserDetail } from "react-icons/bi";
+import { FaUserEdit } from "react-icons/fa";
+import { RiPlayListAddFill } from "react-icons/ri";
 
 const Dashboard = () => {
   const [tab, setTab] = useState("overview");
+
+  const activeTabClass = "bg-indigo-100 text-primaryColor";
+  const inactiveTabClass = "bg-transparent text-headingColor";
 
   const { data, loading, error } = useGetProfile(
     `${BASE_URL}/doctors/profile/me`
   );
 
   return (
-    <section className="py-2">
-      <div className="max-w-[1170px] px-5 mx-auto">
-        {loading && !error && <Loading />}
+    <section className="max-w-[1220px] px-5 mx-auto my-6">
+      {loading && !error && <Loading />}
 
-        {error && !loading && <Error errMessage={error} />}
+      {error && !loading && <Error errMessage={error} />}
 
-        {!loading && !error && (
-          <div className="grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]">
-            {/* <Tabs
-              className="w-full lg:w-[25%] px-[20px] pb-3 rounded-md shadow-md"
-              tab={tab}
-              setTab={setTab}
-            />
-            <div className="lg:w-[">
-              {data.isApproved == "pending" && (
-                <div className="flex justify-center items-center py-3 my-4 text-white bg-red-400 rounded-lg animate-bounce">
-                  <CgDanger className="w-5 h-5" />
+      {!loading && !error && (
+        <div className="flex w-full mx-auto">
+          {/* =======================
+                 left side/Tabs
+          =========================== */}
+          <div className="w-[13%] lg:w-[25%] lg:px-[20px] px-1 pb-3 rounded-md shadow-md bg-white h-max lg:mr-0 mr-[6px]">
+            <button
+              onClick={() => setTab("overview")}
+              className={`${
+                tab == "overview" ? activeTabClass : inactiveTabClass
+              } w-full mt-0 rounded-md flex items-center justify-center lg:px-0 px-1`}
+            >
+              <BiSolidUserDetail className="w-7 h-7" />
+              <p className="ml-3 hidden lg:block">Overview</p>
+            </button>
+            <button
+              onClick={() => setTab("appointments")}
+              className={`${
+                tab == "appointments" ? activeTabClass : inactiveTabClass
+              } w-full lg:mt-0 mt-2 rounded-md flex items-center justify-center lg:px-0 px-1`}
+            >
+              <RiPlayListAddFill className="w-7 h-7" />
+              <p className="ml-3 hidden lg:block">Appointments</p>
+            </button>
+            <button
+              onClick={() => setTab("settings")}
+              className={`${
+                tab == "settings" ? activeTabClass : inactiveTabClass
+              } w-full lg:mt-0 mt-2 rounded-md flex items-center justify-center lg:px-0 px-1`}
+            >
+              <FaUserEdit className="w-7 h-7" />
+              <p className="ml-3 hidden lg:block">Profile</p>
+            </button>
 
-                  <span className="sr-only">Info</span>
-                  <div className="ml-3 text-sm font-medium">
-                    To get approval please complete your profile. We&apos;ll
-                    review manually and approve within 3days.
+            <div className="lg:block hidden mt-[100px] w-full">
+              <button className="lg:text-[16px] text-[14px] rounded-md w-full bg-red-600 mt-4 text-white lg:p-3 p-1">
+                Delete Account
+              </button>
+            </div>
+          </div>
+
+          {/* =======================
+           right side/details layout
+          =========================== */}
+          <div className="w-[87%] lg:w-[75%] lg:ml-8 rounded-md lg:rounded-none shadow-md lg:shadow-none ">
+            {data.isApproved == "pending" && (
+              <div className="flex justify-center items-center py-3 my-4 text-white bg-red-400 rounded-lg animate-bounce">
+                <CgDanger className="w-5 h-5" />
+
+                <span className="sr-only">Info</span>
+                <div className="ml-3 text-sm font-medium">
+                  To get approval please complete your profile. We&apos;ll
+                  review manually and approve within 3days.
+                </div>
+              </div>
+            )}
+
+            <div className="lg:px-0 px-[10px]">
+              {tab == "overview" && (
+                <div>
+                  <div className="lg:flex items-center gap-4 lg:mb-3">
+                    <figure className="lg:max-w-[200px] max-w-[150px] lg:max-h-[200px] max-h-[150px]">
+                      <img
+                        src={data?.photo ? data?.photo : doctorAvatar}
+                        alt=""
+                        className="w-full lg:rounded-none rounded-full"
+                      />
+                    </figure>
+
+                    <div>
+                      <h3 className="font-bold text-[22px] text-headingColor">
+                        {data.name}
+                      </h3>
+                      <p className="text-textColor font-semibold text-[12px] lg:text-[14px]">
+                        {data.specialization ? (
+                          data.specialization
+                        ) : (
+                          <p className="text-red-600 animate-pulse">
+                            Specialization profile isn&apos;t updated. Please
+                            update your Specialization profile.
+                          </p>
+                        )}
+                      </p>
+
+                      <div className="flex items-center gap-[6px] mt-[6px]">
+                        <span
+                          className="flex items-center gap-[6px] text-headingColor font-semibold leading-5 text-[14px] lg:leading-6 lg:text-[16px] 
+                          "
+                        >
+                          <img src={starIcon} alt="" /> {data.averageRating}
+                        </span>
+                        <span
+                          className="text-textColor font-semibold leading-5 text-[14px] lg:leading-6 lg:text-[16px] 
+                          "
+                        >
+                          ({data.totalRating})
+                        </span>
+                      </div>
+                      <p className="text_para font-[15px] lg:max-w-[390px] text-justify leading-6">
+                        {data?.bio}
+                      </p>
+                    </div>
                   </div>
+                  <DoctorAbout
+                    name={data?.name}
+                    about={data?.about}
+                    qualifications={data?.qualifications}
+                    experiences={data?.experiences}
+                  />
                 </div>
               )}
-
-              <div className="mt-4">
-                {tab == "overview" && (
-                  <div>
-                    <div className="flex items-center gap-4 mb-3">
-                      <figure className="max-w-[200px] max-h-[200px]">
-                        <img
-                          src={data?.photo ? data?.photo : doctorAvatar}
-                          alt=""
-                          className="w-full"
-                        />
-                      </figure>
-
-                      <div>
-                        <h3 className="font-bold text-[22px] text-headingColor">
-                          {data.name}
-                        </h3>
-                        <p className="text-textColor font-semibold text-[12px] lg:text-[14px]">
-                          {data.specialization ? (
-                            data.specialization
-                          ) : (
-                            <p className="text-red-600 animate-pulse">
-                              Specialization profile isn&apos;t updated. Please
-                              update your Specialization profile.
-                            </p>
-                          )}
-                        </p>
-
-                        <div className="flex items-center gap-[6px] mt-[6px]">
-                          <span
-                            className="flex items-center gap-[6px] text-headingColor font-semibold leading-5 text-[14px] lg:leading-6 lg:text-[16px] 
-                          "
-                          >
-                            <img src={starIcon} alt="" /> {data.averageRating}
-                          </span>
-                          <span
-                            className="text-textColor font-semibold leading-5 text-[14px] lg:leading-6 lg:text-[16px] 
-                          "
-                          >
-                            ({data.totalRating})
-                          </span>
-                        </div>
-                        <p className="text_para font-[15px] lg:max-w-[390px] text-justify leading-6">
-                          {data?.bio}
-                        </p>
-                      </div>
-                    </div>
-                    <DoctorAbout
-                      name={data?.name}
-                      about={data?.about}
-                      qualifications={data?.qualifications}
-                      experiences={data?.experiences}
-                    />
-                  </div>
-                )}
-                {tab == "appointments" && (
-                  <Appointments appointments={data.appointments} />
-                )}
-                {tab == "settings" && <Profile doctorData={data} />}
-              </div>
-            </div> */}
+              {tab == "appointments" && (
+                <Appointments appointments={data.appointments} />
+              )}
+              {tab == "settings" && <Profile doctorData={data} />}
+            </div>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </section>
   );
 };
