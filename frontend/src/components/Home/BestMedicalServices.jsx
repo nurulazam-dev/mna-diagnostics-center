e7533aebdf4308de41a98528489e7ca209717465
mnaofficialbd <mna.official92@gmail.com>
2024-11-02 00:27:17 +0600

    BestMedicalServices component update-4

--- frontend/src/components/Home/BestMedicalServices.jsx
+++ frontend/src/components/Home/BestMedicalServices.jsx
@@ -30,37 +30,35 @@ const BestMedicalServices = () => {
   ];
 
   return (
-    <section>
-      <div className="container">
-        <h2 className="heading text-center">Best Medical Services</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
-          {providingData.map((item) => (
-            <div key={item.id} className="py-[15px] px-5">
-              <div className="flex items-center justify-center">
-                <img src={item.icon} alt="" />
-              </div>
-
-              <div className="mt-[10px]">
-                <div className="flex justify-center items-center">
-                  <p className="text-[26px] leading-9 text-headingColor font-[700] ">
-                    {item.title}
-                  </p>
-                  <div className="">
-                    <Link
-                      to={item.link}
-                      className="w-[30px] h-[30px] rounded-full border border-solid bottom-[#181A1E] ml-[10px] mx-auto flex items-center justify-center group hover:bg-violet-600 hover:border-none"
-                    >
-                      <BsArrowRight className="group-hover:text-white w-5 h-5 " />
-                    </Link>
-                  </div>
-                </div>
-                <p className="text-[16px] leading-7 text-textColor font-[400] mt-2 text-center">
-                  {item.desc}
-                </p>
-              </div>
+    <section className="container">
+      <h2 className="heading text-center">Best Medical Services</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
+        {providingData.map((item) => (
+          <div key={item.id} className="px-5">
+            <div className="flex items-center justify-center">
+              <img src={item.icon} alt="" />
             </div>
-          ))}
-        </div>
+
+            <div>
+              <div className="flex justify-center items-center">
+                <p className="text-[26px] leading-9 text-headingColor font-[700] ">
+                  {item.title}
+                </p>
+                <div className="">
+                  <Link
+                    to={item.link}
+                    className="w-[30px] h-[30px] rounded-full border border-solid bottom-[#181A1E] ml-[10px] mx-auto flex items-center justify-center group hover:bg-violet-600 hover:border-none"
+                  >
+                    <BsArrowRight className="group-hover:text-white w-5 h-5 " />
+                  </Link>
+                </div>
+              </div>
+              <p className="text-[14px] text-textColor font-[400] mt-1 text-justify">
+                {item.desc}
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );
