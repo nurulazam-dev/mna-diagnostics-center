d22ce579ba0b5fcfd1b4b60c8036c57748d5d86f
mnaofficialbd <mna.official92@gmail.com>
2024-11-02 21:07:28 +0600

    InfiniteScrollBar component setup

--- frontend/src/components/Home/InfiniteScrollBar.jsx
+++ frontend/src/components/Home/InfiniteScrollBar.jsx
@@ -3,16 +3,16 @@ const InfiniteScrollBar = () => {
     "Welcome to MNA Diagnostics Center - Your trusted partner for healthcare services. Book your appointments easily and get the best diagnostic solutions!";
 
   return (
-    <div className="overflow-hidden whitespace-nowrap w-full bg-blue-100 py-2">
+    <div className="overflow-hidden whitespace-nowrap w-full bg-blue-100 py-1">
       <div
         className="inline-block text-black text-[25px] font-serif leading-10"
         style={{
           display: "inline-block",
           whiteSpace: "nowrap",
-          animation: "scroll 320s linear infinite",
+          animation: "scroll 120s linear infinite",
         }}
       >
-        {Array(50).fill(`${noticeText} \u00A0\u00A0`)}
+        {Array(5).fill(`${noticeText} \u00A0\u00A0`)}
       </div>
       <style>
         {`
