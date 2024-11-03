e5d0c30b4e9ee55202114fd51a1b49fa74cc1638
mnaofficialbd <mna.official92@gmail.com>
2024-11-03 13:09:15 +0600

    HealthPackages component setup-2

--- frontend/src/components/Home/HealthPackages.jsx
+++ frontend/src/components/Home/HealthPackages.jsx
@@ -46,29 +46,32 @@ const HealthPackages = () => {
     },
   ];
   return (
-    <div className="bg-gray-50 py-10 px-6 lg:px-24">
-      <h2 className="text-4xl font-bold text-center text-indigo-700 mb-12">
-        Our Health Packages
-      </h2>
+    <section className="px-6 lg:px-24">
+      <h2 className="heading text-center">Our Health Packages</h2>
       <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
         {packages.map((pkg, index) => (
           <div
             key={index}
-            className="bg-white border border-indigo-200 rounded-lg shadow-lg hover:shadow-2xl transition duration-300 overflow-hidden"
+            className="bg-white border border-indigo-200 shadow-lg hover:shadow-2xl transition duration-300 overflow-hidden relative"
           >
+            <div className="absolute -top-3 -right-3 bg-green-600 text-white pl-3 pb-1 pt-3 pr-5 rounded-2xl text-[24px] font-bold shadow-md">
+              {pkg.price}
+            </div>
+
             <img
               src={pkg.image}
               alt={pkg.name}
               className="w-full h-48 object-cover"
             />
-            <div className="p-6 pt-2 flex flex-col justify-between">
-              <h3 className="text-[20px] lg:text-[24px] font-bold text-indigo-800 mb-1 text-center">
+
+            <div className="p-6 py-2 flex flex-col justify-between">
+              <h3 className="text-[20px] lg:text-[25px] font-bold text-indigo-800 mb-1 text-center">
                 {pkg.name}
               </h3>
               <p className="text-[16px] text-gray-600 mb-3 text-justify">
                 {pkg.description}
               </p>
-              <ul className="text-gray-500 pl-2 mb-4 space-y-1 text-[14px]">
+              <ul className="text-gray-500 pl-2 mb-2 space-y-1 text-[14px]">
                 {pkg.facilities.map((facility, idx) => (
                   <li key={idx} className="flex items-center">
                     <IoMdCheckmarkCircle className="w-4 h-4 text-green-600 mr-1" />
@@ -76,19 +79,14 @@ const HealthPackages = () => {
                   </li>
                 ))}
               </ul>
-              <div className="flex items-center justify-between">
-                <p className="text-[24px] font-bold text-violet-700">
-                  {pkg.price}
-                </p>
-                <button className="px-4 py-2 bg-violet-700 text-white rounded-md font-medium hover:bg-green-700 transition-colors">
-                  Book Now
-                </button>
-              </div>
+            </div>
+            <div className="bg-violet-700 text-white font-medium hover:bg-green-700 transition-colors text-center py-3 text-xl">
+              Book Now
             </div>
           </div>
         ))}
       </div>
-    </div>
+    </section>
   );
 };
 
