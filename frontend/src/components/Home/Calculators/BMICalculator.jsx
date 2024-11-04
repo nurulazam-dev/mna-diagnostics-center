46ceb0fdea64193d6277dc28124fe83c22744912
mnaofficialbd <mna.official92@gmail.com>
2024-11-04 20:58:32 +0600

    BMICalculator component setup-3

--- frontend/src/components/Home/Calculators/BMICalculator.jsx
+++ frontend/src/components/Home/Calculators/BMICalculator.jsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
+import bgImg from "../../../assets/images/bgImg.png";
 
 const BMICalculator = () => {
   const [weight, setWeight] = useState("");
   const [height, setHeight] = useState("");
   const [bmi, setBmi] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const calculateBMI = () => {
     if (weight && height) {
       const heightInMeters = height / 100;
       const bmiValue = (weight / (heightInMeters * heightInMeters)).toFixed(2);
       setBmi(bmiValue);
+      setIsModalOpen(true);
     }
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
@@ -41,10 +48,36 @@ const BMICalculator = () => {
       >
         Calculate BMI
       </button>
-      {bmi && (
+
+      {/* ===result part=== */}
+      {/* {bmi && (
         <p className="text-center mt-4 text-lg font-semibold">
           Your BMI is: <span className="text-blue-600">{bmi}</span>
         </p>
+      )} */}
+
+      {/* =======Modal===== */}
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div
+            className="p-6 rounded shadow-lg text-center max-w-sm w-full"
+            style={{
+              backgroundImage: `url(${bgImg})`,
+            }}
+          >
+            <h3 className="text-[24px] text-violet-700 font-bold mb-4 bg-white">
+              Calculate BMI
+            </h3>
+            <p className="text-lg">Your BMI is: </p>
+            <h3 className="text-red-600 text-2xl font-bold">{bmi}</h3>{" "}
+            <button
+              onClick={closeModal}
+              className="mt-6 w-full bg-violet-600 text-white font-bold py-2 animate-bounce hover:bg-red-600"
+            >
+              Close
+            </button>
+          </div>
+        </div>
       )}
     </div>
   );
