a64654432b7376fe7572a74a3233cac3e041de48
mnaofficialbd <mna.official92@gmail.com>
2024-11-06 12:01:35 +0600

    components primary setup-3

--- frontend/src/components/Home/DownloadApp.jsx
+++ frontend/src/components/Home/DownloadApp.jsx
@@ -8,20 +8,20 @@ const DownloadApp = () => {
       <h1 className="heading text-center">Our Mobile App</h1>
       <div className="lg:flex items-center justify-center px-8 w-full">
         {/* =====Mobile Phone Image===== */}
-        <div className="lg:w-[40%] w-full flex justify-center lg:justify-end mb-6 md:mb-0">
+        <div className="lg:w-[40%] w-full flex justify-center lg:justify-end ">
           <img
             src={mobileFrame}
             alt="Mobile App"
-            className="w-48 h-auto md:w-72"
+            className="w-52 h-auto lg:w-72"
           />
         </div>
 
         {/* =====Details and App Links===== */}
         <div className="lg:w-[60%] w-full text-center md:text-left">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+          <h2 className="lg:text-[22px] text-[20px] font-serif font-semibold text-gray-800 ">
             Download the MNA Diagnostics Center App
           </h2>
-          <p className="text-gray-600 mb-6">
+          <p className="text-gray-600 mb-3 lg:text-[16px] text-[14px] text-justify">
             Book appointments, view health packages, and access all our services
             right at your fingertips. Get the app to stay connected and manage
             your health on the go!
@@ -46,7 +46,7 @@ const DownloadApp = () => {
             </div>
           </div>
           {/* =====App Features===== */}
-          <ul className="text-gray-600 space-y-2">
+          <ul className="text-gray-600 space-y-2 text-start">
             <li>✔️ Book Appointments Instantly</li>
             <li>✔️ Get Lab Results Online</li>
             <li>✔️ Easy doctor booking</li>
