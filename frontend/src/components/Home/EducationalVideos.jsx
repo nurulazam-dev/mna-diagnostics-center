ac376fdd807b938719afff73517953547a111cae
mnaofficialbd <mna.official92@gmail.com>
2024-11-09 11:09:39 +0600

    EducationalVideos component  update

--- frontend/src/components/Home/EducationalVideos.jsx
+++ frontend/src/components/Home/EducationalVideos.jsx
@@ -21,39 +21,37 @@ const EducationalVideos = () => {
   ];
 
   return (
-    <section>
-      <div className="container mx-auto px-6 lg:px-12">
-        <h2 className="heading text-center">Educational Videos & Webinars</h2>
-        <div className="grid gap-8 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
-          {videos.map((video) => (
-            <div
-              key={video.id}
-              className="relative group rounded-lg shadow-lg overflow-hidden transform transition-transform duration-300 hover:scale-105"
-            >
-              <iframe
-                className="w-full h-48 object-cover"
-                src={video.videoLink}
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                referrerPolicy="strict-origin-when-cross-origin"
-                allowFullScreen
-              ></iframe>
+    <section className="container mb-10">
+      <h2 className="heading text-center">Educational Videos & Webinars</h2>
+      <div className="grid gap-8 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+        {videos.map((video) => (
+          <div
+            key={video.id}
+            className="relative group rounded-lg shadow-lg overflow-hidden transform transition-transform duration-300 hover:scale-105"
+          >
+            <iframe
+              className="w-full h-48 object-cover"
+              src={video.videoLink}
+              title="YouTube video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerPolicy="strict-origin-when-cross-origin"
+              allowFullScreen
+            ></iframe>
 
-              <div className="p-4 pt-0">
-                <h3 className="text-[20px] font-bold text-violet-600">
-                  {video.title}
-                </h3>
-                <button
-                  to={video.videoLink}
-                  rel="noopener noreferrer"
-                  className="inline-block px-4 py-2 mt-2 text-sm font-medium text-white bg-green-600 rounded hover:bg-violet-600 hover:text-white transition duration-300"
-                >
-                  Watch Now &rarr;
-                </button>
-              </div>
+            <div className="p-4 pt-0">
+              <h3 className="text-[20px] font-bold text-violet-600">
+                {video.title}
+              </h3>
+              <button
+                to={video.videoLink}
+                rel="noopener noreferrer"
+                className="inline-block px-4 py-2 mt-2 text-sm font-medium text-white bg-green-600 rounded hover:bg-violet-600 hover:text-white transition duration-300"
+              >
+                Watch Now &rarr;
+              </button>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </section>
   );
