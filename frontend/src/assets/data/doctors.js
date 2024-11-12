0af0d11cf3d8a80d98d413244b8696350cab1889
mnaofficialbd <mna.official92@gmail.com>
2024-11-13 00:03:55 +0600

    doctors  assets upadate

--- frontend/src/assets/data/doctors.js
+++ frontend/src/assets/data/doctors.js
@@ -1,36 +1,126 @@
-import doctorImg01 from "../images/doctor-img01.png";
-import doctorImg02 from "../images/doctor-img02.png";
-import doctorImg03 from "../images/doctor-img03.png";
+import dr1 from "../images/doctors/dr1.png";
+import dr2 from "../images/doctors/dr2.png";
+import dr3 from "../images/doctors/dr3.png";
+import dr4 from "../images/doctors/dr4.png";
+import dr5 from "../images/doctors/dr5.png";
+import dr6 from "../images/doctors/dr6.png";
+import dr7 from "../images/doctors/dr7.png";
+import dr8 from "../images/doctors/dr8.png";
+import dr9 from "../images/doctors/dr9.png";
+import dr10 from "../images/doctors/dr10.png";
+import dr11 from "../images/doctors/dr11.png";
 
 export const doctors = [
   {
-    id: "01",
-    name: "Dr. Alfaz Ahmed",
-    specialization: "Surgeon",
+    name: "Dr. Kamal Chowdhury",
+    email: "kamal@mnadiagnostics.com",
+    specialization: "Cardiology",
     avgRating: 4.8,
-    totalRating: 272,
-    photo: doctorImg01,
-    totalPatients: 1500,
-    hospital: "Mount Adora Hospital, Sylhet.",
+    totalRating: 150,
+    photo: dr1,
+    totalPatients: 1200,
+    hospital: "City Heart Clinic",
   },
   {
-    id: "02",
-    name: "Dr. Saleh Mahmud",
-    specialization: "Neurologist",
-    avgRating: 4.8,
-    totalRating: 272,
-    photo: doctorImg02,
-    totalPatients: 1500,
-    hospital: "Mount Adora Hospital, Sylhet.",
+    name: "Dr. Neha Sharma",
+    email: "neha@mnadiagnostics.com",
+    specialization: "Pediatrics",
+    avgRating: 4.6,
+    totalRating: 140,
+    photo: dr2,
+    totalPatients: 600,
+    hospital: "Modern Health Clinic",
   },
   {
-    id: "03",
-    name: "Dr. Farid Uddin",
-    specialization: "Dermatologist",
+    name: "Dr. Ratul Khan",
+    email: "ratul@mnadiagnostics.com",
+    specialization: "Dentistry",
+    avgRating: 4.9,
+    totalRating: 180,
+    photo: dr3,
+    totalPatients: 700,
+    hospital: "Happy Kids Hospital",
+  },
+  {
+    name: "Dr. Mahmudur Rahaman",
+    email: "mahmud@mnadiagnostics.com",
+    specialization: "Neurology",
+    avgRating: 4.6,
+    totalRating: 200,
+    photo: dr4,
+    totalPatients: 950,
+    hospital: "Neuro Medical Hospital",
+  },
+
+  {
+    name: "Dr. Shahab Uddin",
+    email: "shahab@mnadiagnostics.com",
+    specialization: "Urology",
+    avgRating: 4.7,
+    totalRating: 160,
+    photo: dr5,
+    totalPatients: 480,
+    hospital: "Kidney Health Clinic",
+  },
+  {
+    name: "Dr. Shirin Khan",
+    email: "shirin@mnadiagnostics.com",
+    specialization: "Orthopedics",
+    avgRating: 4.7,
+    totalRating: 140,
+    photo: dr6,
+    totalPatients: 850,
+    hospital: "Bone Care Clinic",
+  },
+  {
+    name: "Dr. Shaki Akther",
+    email: "shaki@mnadiagnostics.com",
+    specialization: "Dermatology",
+    avgRating: 4.9,
+    totalRating: 154,
+    photo: dr7,
+    totalPatients: 972,
+    hospital: "Super Care Hospital",
+  },
+  {
+    name: "Dr. Arif Khan",
+    email: "arif@mnadiagnostics.com",
+    specialization: "Gastroenterology",
+    avgRating: 4.6,
+    totalRating: 140,
+    photo: dr8,
+    totalPatients: 600,
+    hospital: "Digestive Health Center",
+  },
+  {
+    name: "Dr. Rina Khanum",
+    email: "rina@mnadiagnostics.com",
+    specialization: "Ophthalmology",
     avgRating: 4.8,
-    totalRating: 272,
-    photo: doctorImg03,
-    totalPatients: 1500,
-    hospital: "Mount Adora Hospital, Sylhet.",
+    totalRating: 180,
+    photo: dr9,
+    totalPatients: 550,
+    hospital: "VisionCare Eye Hospital",
+  },
+
+  {
+    name: "Dr. Anjali Mehta",
+    email: "anjali@mnadiagnostics.com",
+    specialization: "Oncology",
+    avgRating: 4.9,
+    totalRating: 210,
+    photo: dr10,
+    totalPatients: 600,
+    hospital: "Cancer Care Hospital",
+  },
+  {
+    name: "Dr. Amrita Khan",
+    email: "amrita@mnadiagnostics.com",
+    specialization: "Gynecology",
+    avgRating: 4.8,
+    totalRating: 220,
+    photo: dr11,
+    totalPatients: 1100,
+    hospital: "Women's Wellness Center",
   },
 ];
